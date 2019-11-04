6acb62c42e21a70bcfcf0a17ba9f25ab49d3eda6
GabriellyEZ <gabrielly23ezequiel@gmail.com>
2019-11-04 20:40:30 -0300

    inserir no banco de dados

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const exp = express();
-const Usuario = require('./models/bd/table/Usuario')
+const Usuario = require('./models/bd/table/Usuario');
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 //vamos setar no express o nosso handlebars
     exp.engine('handlebars',handlebars({defaultLayout: 'main'}))
     exp.set('view engine','handlebars')
+    //configurando bodyParse
+    exp.use(bodyParser.urlencoded({extended:false}))
+    exp.use(bodyParser.json())
 
-  //rotas
+  //rotas 
     exp.get("/",(req,res) => {
-        Usuario.findAll().then((Usuarios) => {
+        Usuario.findAll({order: [['id','DESC']]}).then((Usuarios) => {
         res.render('listagem',{bd_usuarios: Usuarios})
         })
     })
 
     exp.get("/Cadastro",(req,res)=>{
-        res.send("TA FUNCIONANDO OTARIO")
+        res.render('formulario')
+    })
+
+    exp.post("/add",(req,res)=>{
+        Usuario.create({
+            NomeDeLogin:req.body.NomeDeLogin,
+            Email:req.body.Email,
+            Senha:req.body.Senha
+        }).then(() =>{
+            res.redirect("/")
+        }).catch((e)=>{
+            res.send("Falha ao cadastrar."+e)
+        })
+        
+        
     })
 
 exp.listen(8081,function(){
