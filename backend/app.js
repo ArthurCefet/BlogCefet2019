1210b2ff8f74a10af9f10a93e665e4bac10b925b
ArthurCefet <arthurpedrosaferreira@hotmail.com>
2019-11-03 23:21:04 -0300

    Atualizacao_ROTAS

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,20 @@
-const ListarTodosUsuarios = require('./models/usuario/action/listarTodosUsuarios')
+
 const ListarUmUsuarioByID = require('./models/usuario/action/ListarUmUsuarioByID')
+const express = require('express');
+const exp = express();
+const Usuario = require('./models/bd/table/Usuario')
+
+
+exp.get("/",(req,res) => {
+    Usuario.findAll().then((Usuario) => {
+    res.send(JSON.stringify(Usuario,null,4))
+    })
+  })
+
+exp.get("/Inserir",(req,res)=>{
+    
+})
+
+exp.listen(8081,function(){
+    console.log("Servido rodando na porta 8081");
+  });
