229f4544c2b4efad11f14a63f258263d6a8f6d32
ArthurCefet <arthurpedrosaferreira@hotmail.com>
2019-11-06 23:15:36 -0300

    Arthur _Funcionalidade_Deletar

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,13 @@ const Usuario = require('./models/bd/table/Usuario');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 
-//vamos setar no express o nosso handlebars
+//handlebars é um processador de template que torna a pagina dinamica
+    //vamos setar no express o nosso handlebars
     exp.engine('handlebars',handlebars({defaultLayout: 'main'}))
     exp.set('view engine','handlebars')
-    //configurando bodyParse
-    exp.use(bodyParser.urlencoded({extended:false}))
+
+//bodyparser é usado para converter dados de uma requisição
+    //nesse caso é convertido os dados da requisicao para json
     exp.use(bodyParser.json())
 
   //rotas 
@@ -32,9 +34,20 @@ const bodyParser = require('body-parser');
         }).catch((e)=>{
             res.send("Falha ao cadastrar."+e)
         })
-        
-        
     })
+
+    //vamos criar uma rota para deletar determinado usuario pelo id
+        // O valor do id é recebido via get pela URL , repare que diferente do criar usuario o parametro não é pego pelo formulario html, nessa caso o vlaor é pego na url (req.params.id)
+    exp.get("/remover/:id",(req,res)=>{
+        Usuario.destroy({ where: {'id':req.params.id}}).then(()=>{
+            res.redirect("/")
+            console.log("Sucesso ao remover o usuario de id : "+req.params.id)
+        }).catch((e)=>{
+            console.log("ERROR AO DELETAR . "+ e)
+        })
+    })
+
+
 
 exp.listen(8081,function(){
     console.log("Servido rodando na porta 8081");
