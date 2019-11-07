d31b61a42c031bf64b42682553206b2b5f394f76
Leonardo de Almeida Teodoro <leonardoGithub@gmail.com>
2019-11-07 04:22:43 -0300

    Update Bd.js

--- backend/models/bd/Bd.js
+++ backend/models/bd/Bd.js
@@ -1,16 +1,16 @@
-//Importar o Sequelize
-    //A gente cria a variavel como const por uma medida de segurança para evitar que alguem ou algo mude o valor da variavel
+//Importação do Sequelize
+    //Criamos a variavel como const por uma medida de segurança para evitar que alguém ou algo mude o valor da variável
 const Sequelize = require("sequelize")
 
-//Vamos criar a conexão com o bd usando o SEQUELIZE
-    //como o Sequelize não é especifico de 1 banco de dados o campo "Dialect" especifica qual bd vai ser utilizado
+//Criação da conexão com o bd usando o SEQUELIZE
+    //como o Sequelize não é especifico de um banco de dados, o campo "Dialect" especifica qual bd será utilizado
 const conexao = new Sequelize('crudusuario','root','', {
     host: 'localhost',
     dialect: 'mysql',
     logging: false
 })
 
-//Avalia se a conexao com o banco de dados foi feita
+//Avalia se a conexao com o banco de dados foi realizada
 conexao.authenticate().then(()=>{
     console.log("Conexao Com o Banco de Dados Realizada!")
 }).catch((e)=>{
@@ -18,7 +18,7 @@ conexao.authenticate().then(()=>{
 })
 
 
-//agora que a conexao com o bd foi realizada, ela vai ser exportada para outras areas do sistema
+//agora que a conexao com o bd foi realizada, ela será exportada para outras áreas do sistema
 
 module.exports = {
     Sequelize : Sequelize,
